c00b76ce67e71a6296f337be08c7210525b084e2
yuanhaokai <yuanhaokai@meituan.com>
2023-11-05 22:05:37 +0800

    feat:增删改查接口完成,数据库调通

--- routes/talent.js
+++ routes/talent.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const sqlite3 = require('sqlite3').verbose();
-router.post('/getTalentData', (req, res) => {
-    console.log('131313');
-    res.send({ message: 'success', data: [
-        {
-          key: '1',
-          name: '张三',
-          age: 32,
-          address: '北京市',
-        },
-        {
-          key: '2',
-          name: '李四',
-          age: 42,
-          address: '上海市',
-        }
-      ]});
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: "./dataBase/db.sqlite3"
+    }
+});
+    router.post('/getTalentData', (req, res) => {
+        knex('StaffManage_userinfo').where(req.body).then(rows => {
+            res.send({ message: 'success', data: rows});
+        }).catch(err => {
+            console.log(err);
+            res.send({ message: 'error', data: []});
+        });
     });
+    router.post('/addTalent', (req, res) => {
+        knex('StaffManage_userinfo').insert(req.body).then(() => {
+            res.send({ message: 'success'});
+        }).catch(err => {
+            console.log(err);
+            res.send({ message: 'error'});
+        });
+    });
+    router.delete('/deleteTalent', (req, res) => {
+        knex('StaffManage_userinfo').where('id', req.body.id).del().then(() => {
+            res.send({ message: 'success'});
+        }).catch(err => {
+            console.log(err);
+            res.send({ message: 'error'});
+        });
+    }); 
+    router.put('/updateTalent', (req, res) => {
+        knex('StaffManage_userinfo').where('id', req.body.id).update(req.body).then(() => {
+            res.send({ message: 'success'});
+        }).catch(err => {
+            console.log(err);
+            res.send({ message: 'error'});
+        });
+    });
+    
 module.exports = router;
