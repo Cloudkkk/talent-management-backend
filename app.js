9b9a16ed604c00f03dc62d662c03acbaf72b21d7
yuanhaokai <yuanhaokai@meituan.com>
2023-11-05 19:43:03 +0800

    feat:查询接口调通[FE->controller]

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const talentRoutes = require('./routes/talent');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/talent', talentRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 });
